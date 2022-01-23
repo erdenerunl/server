f54505d4369d7248191c9f452665489b3331319a
erdener <erdenerunale@gmail.com>
2022-01-23 22:42:22 +0300

    post data revized.

--- controllers/getPosts.js
+++ controllers/getPosts.js
@@ -3,7 +3,12 @@ import Post from "../models/posts.js";
 export const getPosts = async (req, res) => {
     try {
         const posts = await Post.find();
-        res.status(200).json(posts)
+        res.status(200).json({
+            status: true,
+            data: {
+                posts: posts
+            }
+        })
     } catch (err) {
         res.status(404).json({
             status: false,
